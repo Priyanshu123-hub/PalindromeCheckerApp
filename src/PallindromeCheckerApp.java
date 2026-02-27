1966427a8f55d4043f0fdefd299167176dc3a2b9
Priyanshu123-hub <priyanshusaha243@gmail.com>
2026-02-27 15:52:21 +0530

    UC5:Stack based palindrome checker

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -1,32 +1,32 @@
+
 public class PallindromeCheckerApp {
     public static void main(String[] args) {
 
+        // Hardcoded string
         String original = "madam";
 
-        // Convert string to character array
-        char[] characters = original.toCharArray();
+        // Create a stack
+        java.util.Stack<Character> stack = new java.util.Stack<>();
 
-        int left = 0;
-        int right = characters.length - 1;
-        boolean isPalindrome = true;
-
-        // Two-pointer comparison
-        while (left < right) {
-            if (characters[left] != characters[right]) {
-                isPalindrome = false;
-                break;
-            }
-            left++;
-            right--;
+        // Push all characters into the stack
+        for (int i = 0; i < original.length(); i++) {
+            stack.push(original.charAt(i));
         }
 
-        // Display result
-        if (isPalindrome) {
+        // Pop characters to build reversed string
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        // Compare original and reversed
+        if (original.equals(reversed)) {
             System.out.println("The given string \"" + original + "\" is a Palindrome.");
         } else {
             System.out.println("The given string \"" + original + "\" is NOT a Palindrome.");
         }
     }
+
 
 
 
