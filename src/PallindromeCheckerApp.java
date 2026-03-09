30b57d663c608ee1c8a06cd27ed399fdcfef2c84
Priyanshu123-hub <priyanshusaha243@gmail.com>
2026-03-09 15:07:00 +0530

    UC13:Hardcoded performance palindrome

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -3,39 +3,30 @@ import java.util.LinkedList;
 public class PallindromeCheckerApp {
     public static void main(String[] args) {
 
-        String input = "level";
+        java.util.Scanner sc = new java.util.Scanner(System.in);
 
-        PalindromeStrategy strategy = new StackStrategy();
+        System.out.print("Input : ");
+        String input = sc.nextLine();
 
-        boolean result = strategy.check(input);
+        long startTime = System.nanoTime();
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
-    }
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = input.length() - 1;
 
-}
-
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                isPalindrome = false;
+                break;
             }
+            start++;
+            end--;
         }
 
-        return true;
+        long endTime = System.nanoTime();
+
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + (endTime - startTime) + " ns");
     }
 }
 
